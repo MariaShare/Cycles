98e1f3a7520b9acefc6b7ff01f0855a515146455
Mahadevii <mshadrina@gmail.com>
2023-06-26 06:17:30 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp3/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        ComboBox1 = New ComboBox()
+        ListBox1 = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(187, 82)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(14, 50)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(14, 82)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"#1Типы циклов", "#2Виды циклов"})
+        ComboBox1.Location = New Point(14, 19)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 7
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Location = New Point(15, 132)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(461, 304)
+        ListBox1.TabIndex = 8
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(490, 457)
+        Controls.Add(ListBox1)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ WinFormsApp3/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Введите натуральное число"
+        Button1.Text = "Вычислить"
+    End Sub
+    Private Sub ButEvs(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedIndex = -1 Then
+            MsgBox("Выберите функцию!")
+        Else
+            Dim i = ComboBox1.SelectedIndex
+            Select Case i
+                Case 0
+                    ListBox1.Items.Clear()
+                    NInfo()
+                Case 1
+                    ListBox1.Items.Clear()
+                    sumSin()
+            End Select
+        End If
+    End Sub
+
+    Private Sub NInfo()
+        Dim num = TextBox1.Text
+        Dim i = 0
+        Dim sum2 = 0
+        Dim sum3 = 0
+        Dim fl = 0
+        Do While i < Len(num)
+            sum2 += Math.Pow(Val(num(i)), 2)
+            sum3 += Math.Pow(Val(num(i)), 3)
+            If i = 0 Or i = Len(num) - 1 Then
+                fl += Val(num(i))
+            End If
+            i += 1
+        Loop
+        ListBox1.Items.Add("Пред Do While:" + Str(sum2) + Str(sum3) + Str(Val(num(0))) + Str(fl))
+
+        i = Len(num) - 1
+        sum2 = 0
+        sum3 = 0
+        fl = 0
+        Do Until i < 0
+            sum2 += Math.Pow(Val(num(i)), 2)
+            sum3 += Math.Pow(Val(num(i)), 3)
+            If i = 0 Or i = Len(num) - 1 Then
+                fl += Val(num(i))
+            End If
+            i -= 1
+        Loop
+        ListBox1.Items.Add("Пред Do Until:" + Str(sum2) + Str(sum3) + Str(Val(num(0))) + Str(fl))
+
+        i = 0
+        sum2 = 0
+        sum3 = 0
+        fl = 0
+        Do
+            sum2 += Math.Pow(Val(num(i)), 2)
+            sum3 += Math.Pow(Val(num(i)), 3)
+            If i = 0 Or i = Len(num) - 1 Then
+                fl += Val(num(i))
+            End If
+            i += 1
+        Loop While i < Len(num)
+        ListBox1.Items.Add("Пост Do While:" + Str(sum2) + Str(sum3) + Str(Val(num(0))) + Str(fl))
+
+        i = Len(num) - 1
+        sum2 = 0
+        sum3 = 0
+        fl = 0
+        Do
+            sum2 += Math.Pow(Val(num(i)), 2)
+            sum3 += Math.Pow(Val(num(i)), 3)
+            If i = 0 Or i = Len(num) - 1 Then
+                fl += Val(num(i))
+            End If
+            i -= 1
+        Loop Until i < 0
+        ListBox1.Items.Add("Пост Do Until:" + Str(sum2) + Str(sum3) + Str(Val(num(0))) + Str(fl))
+    End Sub
+    Private Sub sumSin()
+        Dim num = CInt(TextBox1.Text)
+        Dim summ As Double = 0
+        Dim summSin As Double = 0
+        Dim i As Int64
+        For i = 1 To num
+            summSin += Math.Sin(i)
+            summ += 1 / summSin
+        Next i
+        ListBox1.Items.Add("For:" + Str(summSin) + Str(summ))
+
+        summ = 0
+        summSin = 0
+        i = 1
+        Do While i <= num
+            summSin += Math.Sin(i)
+            summ += 1 / summSin
+            i += 1
+        Loop
+        ListBox1.Items.Add("Do While:" + Str(summSin) + Str(summ))
+    End Sub
+End Class
